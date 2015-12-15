0cc1f7217332e1fff01cf19f1c4ba1fd9b580d30
Lennard Westerveld <mail@lennard.pro>
2015-12-15 13:37:39 +0100

    Added randomhash Added config directorys outside the webroot

--- web/sites/default/default.settings.local.php
+++ web/sites/default/default.settings.local.php
@@ -11,3 +11,12 @@ $databases['default']['default'] = array(
 
 # Enable on development.
 # $settings['container_yamls'][] = __DIR__ . '/services.development.yml';
+
+# Random hash
+$settings['hash_salt'] = '__RANDOM_HASH_SALT__';
+
+$config_directories = array(
+  CONFIG_ACTIVE_DIRECTORY => '../config/active/',
+  CONFIG_STAGING_DIRECTORY => '../config/staging/',
+  CONFIG_SYNC_DIRECTORY => '../config/sync/'
+);
